4fc7029ec955d06facf5144b18b8bc43093092a5
Jonathan Ramirez <turel_1988@hotmail.com>
2023-07-31 22:03:08 +0200

    Configuracion de base de datos

--- Gymapp/db/initDB.js
+++ Gymapp/db/initDB.js
@@ -8,43 +8,42 @@ async function main() {
 
     console.log('Borrando tablas existentes');
     await connection.query(`DROP TABLE IF EXISTS activities`);
-    await connection.query();
 
     console.log('Creando tablas)');
     await connection.query(`CREATE TABLE user(
-            id INTEGET PRIMARY KEY AUTO_INCREMENT,
+            id INTEGER PRIMARY KEY AUTO_INCREMENT,
             email VARCHAR(100) UNIQUE NOT NULL,
-            user_name VARCHAR (100) UNIQUE NOT NULL,
-            password VARCHARD (100) NOT NULL,
-            created_at DATATIME DEFAULT CURRENT_TIMESTAMP
+            user_name VARCHAR(100) UNIQUE NOT NULL,
+            password VARCHAR(100) NOT NULL,
+            created_at DATETIME DEFAULT CURRENT_TIMESTAMP
         );
         `);
 
     await connection.query(`CREATE TABLE employees(
-            id INTEGET PRIMARY KEY AUTO_INCREMENT,
-            email VARCHAR (100) UNIQUE NOT NULL,
-            user_name VARCHAR (100) UNIQUE NOT NULL, 
-            password VARCHARD (100) NOT NULL,
-            created_at DATATIME DEFAULT CURRENT_TIMESTAMP
+            id INTEGER PRIMARY KEY AUTO_INCREMENT,
+            email VARCHAR(100) UNIQUE NOT NULL,
+            user_name VARCHAR(100) UNIQUE NOT NULL, 
+            password VARCHAR(100) NOT NULL,
+            created_at DATETIME DEFAULT CURRENT_TIMESTAMP
 
         );
         `);
 
     await connection.query(`CREATE TABLE activities(
-            id INTEGET PRIMARY KEY AUTO_INCREMENT,
-            activity_name VARCHAR (100) UNIQUE NOT NULL,
-            description VARCHAR (300) NOT NULL,
-            image VARCHAR (100), 
-            typology VARCHAR (100),
-            muscle_group VARCHAR (100),
-            created_at DATATIME DEFAULT CURRENT_TIMESTAMP
+            id INTEGER PRIMARY KEY AUTO_INCREMENT,
+            activity_name VARCHAR(100) UNIQUE NOT NULL,
+            description VARCHAR(300) NOT NULL,
+            image VARCHAR(100), 
+            typology VARCHAR(100),
+            muscle_group VARCHAR(100),
+            created_at DATETIME DEFAULT CURRENT_TIMESTAMP
   
         );
         `);
   } catch (error) {
     console.error(error);
   } finally {
-    if (connection) connection.realease();
+    if (connection) connection.release();
     process.exit();
   }
 }
