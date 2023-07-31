16e0a362f955490fb5ad22d62aa7f6d73ceb5e18
Jonathan Ramirez <turel_1988@hotmail.com>
2023-08-01 00:56:29 +0200

    Modificar o eliminar un entrenamiento

--- Gymapp/controllers/activities.js
+++ Gymapp/controllers/activities.js
@@ -1,17 +1,18 @@
 const newActivityController = async (req, res, next) => {
+console.log("controlador nuevas actividades");
 
 }
 
 const getActivityController = (req, res, next) => {
-
+    console.log("controlador para obtener una actividad por ID");
 }
 
 const modifyActivityController =(req, res, next) => {
-
+    console.log("controlador para modificar una actividad por ID");
 }
 
 const deleteActivityController =(req, res, next) => {
-
+    console.log("controlador para borrar una actividad por ID");
 }
 
 
